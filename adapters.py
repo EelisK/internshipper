da16fcbf11b1d2b4dab7bc35fe5311516112f715
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-05-10 21:43:48 +0300

    Cleanup

--- adapters.py
+++ adapters.py
@@ -1,12 +1,11 @@
-from urllib3 import poolmanager
 import ssl
+from urllib3 import poolmanager
 from requests import adapters
 
 
 class TLSAdapter(adapters.HTTPAdapter):
 
     def init_poolmanager(self, connections, maxsize, block=False):
-        """Create and initialize the urllib3 PoolManager."""
         ctx = ssl.create_default_context()
         ctx.set_ciphers('DEFAULT@SECLEVEL=1')
         self.poolmanager = poolmanager.PoolManager(
@@ -14,4 +13,5 @@ class TLSAdapter(adapters.HTTPAdapter):
             maxsize=maxsize,
             block=block,
             ssl_version=ssl.PROTOCOL_TLS,
-            ssl_context=ctx)
+            ssl_context=ctx
+        )
--- jobiili.py
+++ jobiili.py
@@ -9,6 +9,7 @@ from adapters import TLSAdapter
 
 BASE_URL = "https://www.jobiili.fi"
 API_URL = BASE_URL + "/api"
+METROPOLIA_IDP_URL = "https://idp.metropolia.fi/idp/profile/SAML2/Redirect/SSO"
 
 
 class Client:
@@ -20,48 +21,13 @@ class Client:
         self.authorization = None
 
     def login(self):
-        response = self.session.get(BASE_URL)
-        response.raise_for_status()
-
-        response = self.session.get(
-            BASE_URL + "/Shibboleth.sso/Login?SAMLDS=1&&target=ss%3Amem%3A37145caa43527f537365280be5ad3ba0b03419c4fb1f3645bce1864341006721&entityID=https%3A%2F%2Fidp.metropolia.fi%2Fidp"
+        self.__initialize_session_for_login()
+        response = self.__perform_metropolia_idp_login()
+        response = self.__invoke_successful_idp_login_callback(response)
+        self.authorization = self.__parse_authorization_from_login_callback(
+            response
         )
-        response.raise_for_status()
-
-        response = self.session.post(
-            "https://idp.metropolia.fi/idp/profile/SAML2/Redirect/SSO?execution=e1s1",
-            data={
-                "j_username": self.username,
-                "j_password": self.password,
-                "_eventId_proceed": "",
-            },
-        )
-        response.raise_for_status()
-
-        if "The password you entered was incorrect." in response.text:
-            raise InvalidCredentials()
-
-        soup = bs4.BeautifulSoup(response.text, "lxml")
-        form = soup.find("form")
-        data = {
-            e.attrs["name"]: e.attrs["value"]
-            for e in form.find_all("input")
-            if "name" in e.attrs
-        }
-
-        response = self.session.post(
-            BASE_URL + "/Shibboleth.sso/SAML2/POST", data=data)
-        response.raise_for_status()
-
-        location = response.history[0].headers["Location"]
-        self.authorization = location.split("/")[3]
-
-        response = self.session.get(BASE_URL + location)
-        response.raise_for_status()
-
-        response = self.session.get(
-            API_URL + "/People/getTokenDetails?token=" + self.authorization)
-        self.identity = response.json()
+        self.__perform_identity_check()
 
     def get_jobs(self, payload):
         response = requests.get(API_URL + "/Jobs/search?query=" + json.dumps(payload), headers={
@@ -69,3 +35,58 @@ class Client:
         if response.status_code != 200:
             raise AuthenticationException()
         return response.json()
+
+    def __initialize_session_for_login(self):
+        response = self.session.get(BASE_URL)
+        response.raise_for_status()
+        response = self.session.get(
+            BASE_URL + "/Shibboleth.sso/Login?SAMLDS=1&&target=ss%3Amem%3A37145caa43527f537365280be5ad3ba0b03419c4fb1f3645bce1864341006721&entityID=https%3A%2F%2Fidp.metropolia.fi%2Fidp"
+        )
+        response.raise_for_status()
+
+    def __perform_metropolia_idp_login(self):
+        response = self.session.post(
+            METROPOLIA_IDP_URL + "?execution=e1s1",
+            data={
+                "j_username": self.username,
+                "j_password": self.password,
+                "_eventId_proceed": "",
+            },
+        )
+        response.raise_for_status()
+        response_text = response.text
+        if "The password you entered was incorrect." in response_text:
+            raise InvalidCredentials("Incorrect password")
+        if "The username you entered cannot be identified." in response_text:
+            raise InvalidCredentials("Incorrect username.")
+        return response
+
+    def __invoke_successful_idp_login_callback(self, idp_login_response):
+        response = self.session.post(
+            BASE_URL + "/Shibboleth.sso/SAML2/POST",
+            data=self.__parse_sso_login_data(idp_login_response)
+        )
+        response.raise_for_status()
+        return response
+
+    def __parse_sso_login_data(self, idp_login_response):
+        soup = bs4.BeautifulSoup(idp_login_response.text, "lxml")
+        form = soup.find("form")
+        return {
+            e.attrs["name"]: e.attrs["value"]
+            for e in form.find_all("input")
+            if "name" in e.attrs
+        }
+
+    def __parse_authorization_from_login_callback(self, jobiili_callback_response):
+        location = jobiili_callback_response.history[0].headers["Location"]
+        response = self.session.get(BASE_URL + location)
+        response.raise_for_status()
+        # Authorization format: /#/haka-redirect/<token>
+        return location.split("/")[3]
+
+    def __perform_identity_check(self):
+        response = self.session.get(
+            API_URL + "/People/getTokenDetails?token=" + self.authorization)
+        response.raise_for_status()
+        self.identity = response.json()

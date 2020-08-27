6c1e74aed602caaf9653d73e368ffe3327169185
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-08-28 01:21:05 +0300

    Update jobs page

--- client/src/components/Pages/Jobs/JobsPage.tsx
+++ client/src/components/Pages/Jobs/JobsPage.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 import { InternshipForm } from "../../InternshipForm";
-import { Nav, Anchor, Main } from "grommet";
-import { Mail } from "grommet-icons";
+import { Main } from "grommet";
 
 export interface Props {}
 
 export const JobsPage: React.FC<Props> = () => (
-  <Main>
-    {/* <h1>TODO: add some other content than the background</h1> */}
+  <Main flex align="center" pad="large">
     <InternshipForm />
   </Main>
 );

9a8e3d9ba4f3b79d1d44917e9cc796cbfed1b3ad
Eelis Kostiainen <eelis.kostiainen@aalto.fi>
2020-09-15 14:26:03 +0200

    Add error details

--- client/src/components/StateOverlay/StateOverlay.tsx
+++ client/src/components/StateOverlay/StateOverlay.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Box } from "grommet";
+import { Box, Text } from "grommet";
+import { Close } from "grommet-icons";
 import { RequestState, useRequestState } from "../../providers";
 
 export interface Props {
@@ -16,9 +17,32 @@ export const REQUEST_STATUS_STYLES: Record<
   idle: {},
 };
 
-export const StateOverlay: React.FC<Props> = (props) => (
-  <Box style={REQUEST_STATUS_STYLES[props.state.status]}>{props.children}</Box>
-);
+export const StateOverlay: React.FC<Props> = (props) => {
+  const [includeExtraInfo, setIncludeExtraInfo] = React.useState<boolean>(true);
+  React.useEffect(() => {
+    setIncludeExtraInfo(true);
+  }, [props.state]);
+
+  return (
+    <Box style={REQUEST_STATUS_STYLES[props.state.status]}>
+      {includeExtraInfo && props.state.status === "error" && (
+        <Box pad="medium" direction="row">
+          <Box align="center" width="100%">
+            <Text color="status-error">{props.state.error.message}</Text>
+          </Box>
+          <Box
+            style={{ cursor: "pointer" }}
+            onClick={() => setIncludeExtraInfo(false)}
+            margin={{ left: "auto" }}
+          >
+            <Close />
+          </Box>
+        </Box>
+      )}
+      {props.children}
+    </Box>
+  );
+};
 
 export type WrapperProps = Omit<Props, "state">;
 
